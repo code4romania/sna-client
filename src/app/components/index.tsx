3dff043f59a86404ed76949045844783b16a774a
Vitalie Lazu <vitalie.lazu@gmail.com>
2017-03-09 01:21:44 +0200

    Added sample component RomaniaMap
    https://github.com/code4romania/sna-client/issues/6

--- src/app/components/index.tsx
+++ src/app/components/index.tsx
@@ -2,3 +2,4 @@ export { Header } from './Header';
 export { Sidebar } from './Sidebar';
 export { Content } from './Content';
 export { Badge } from './Badge';
+export { RomaniaMap } from './RomaniaMap';
--- src/app/containers/Home/index.tsx
+++ src/app/containers/Home/index.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
+import {RomaniaMap, CountyColorMap} from '../../components/RomaniaMap/index';
 const style = require('./style.css');
 
 class Home extends React.Component<any, any> {
   public render() {
+    const map: CountyColorMap = {
+      arad: 'red', bihor: 'white', alba: '#cfe1f6',
+    };
     return (
       <div className={style.Home}>
-        <p>Hello!</p>
+        <RomaniaMap width={500} colorMap={map} />
       </div>
     );
   }
