458380df1286fe5a5e43b6006744f7f19b9d5509
Vitalie Lazu <vitalie.lazu@gmail.com>
2017-03-15 19:18:35 +0200

    Added recharts

--- config/types/dev.d.ts
+++ config/types/dev.d.ts
@@ -25,3 +25,18 @@ interface SliderProps {
 }
 
 declare class ReactBootstrapSlider extends React.Component<SliderProps, {}> { }
+
+declare module "recharts" {
+  export const BarChart: any;
+  export const Bar: any;
+  export const Tooltip: any;
+  export const Legend: any;
+  export const Area: any;
+  export const AreaChart: any;
+  export const LineChart: any;
+  export const Line: any;
+  export const XAxis: any;
+  export const YAxis: any;
+  export const CartesianGrid: any;
+  export const ResponsiveContainer: any;
+}
