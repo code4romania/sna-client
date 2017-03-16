ff8c95182f755eccb4263054c24b75ae3c956f7e
Sergiu Munteanu <sergiu@meniga.net>
2017-03-16 14:46:23 +0100

    Implemented checkbox component

---
+++ src/app/components/Checkbox/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+const style = require('./style.css');
+
+interface CheckboxProps {
+  checked: boolean;
+  name: string;
+  label: string;
+  value: string;
+}
+
+export const Checkbox = (props: CheckboxProps) => (
+  <label className={style.Checkbox}>
+    <input className={style.CheckboxInput} type="checkbox" name={props.name} value={props.value} />
+    <span className={style.CheckboxIcon} />
+    <span className={style.CheckboxLabel}>{props.label}</span>
+  </label>
+);
