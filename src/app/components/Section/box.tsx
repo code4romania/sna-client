47272d8800d47b5f0ac6317c255634f0560d24e8
Vitalie Lazu <vitalie.lazu@gmail.com>
2017-03-20 09:40:48 +0200

    WIP: First indicator for ministries

--- src/app/components/Section/box.tsx
+++ src/app/components/Section/box.tsx
@@ -3,11 +3,12 @@ import {filterStyle} from "./filters";
 
 interface Props {
   children?: any;
+  className: string;
 }
 
 export function Box(props: Props) {
   return (
-    <div className={filterStyle.box}>
+    <div className={`${filterStyle.box} ${props.className || ""}`}>
       {props.children}
     </div>
   );
--- src/app/containers/Indicators/indicator1_all_years.tsx
+++ src/app/containers/Indicators/indicator1_all_years.tsx
@@ -7,7 +7,19 @@ interface Props {
 export class Indicator1AllYears extends React.Component<Props, any> {
   public render(): JSX.Element {
     return (
-      <Box>TODO All years</Box>
+      <div>
+        <Box className="big_box">
+          <div>Număr de sesizări privind încălcări ale normelor</div>
+          <div>
+            <div>
+              <div>* sesizări soluționate</div>
+              <div>* sesizări în curs de soluționare</div>
+            </div>
+            <div>Chart</div>
+          </div>
+        </Box>
+        <Box className="small_box">TODO All years</Box>
+      </div>
     );
   }
 }
