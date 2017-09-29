0f3ea641a7e9e66f79c29bbbb455a49b7d86af65
Lucian Ursu <lucian.ursu@gmail.com>
2017-09-29 16:04:34 +0300

    Place logo after the org name

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -5,8 +5,8 @@ const style = require('./style.css');
 export const Footer = () => (
   <div className={style.footer}>
     <a href="http://code4.ro" target="_blank">
-      <img className={style.logo} src={require('./brand-code4.png')} />
       <span className={style.byline}>Creat de Code for Romania</span>
+      <img className={style.logo} src={require('./brand-code4.png')} />
     </a>
   </div>
 );
