38881b3c4bdeeddf4ed545519153d141c686256d
Lucian Ursu <lucian.ursu@gmail.com>
2017-12-23 21:34:30 +0200

    Work in progress

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -3,10 +3,15 @@ import * as React from 'react';
 const style = require('./style.css');
 
 export const Footer = () => (
-  <div className={style.footer}>
-    <a href="http://code4.ro" target="_blank">
-      <span className={style.byline}>Creat de Code for Romania</span>
-      <img className={style.logo} src={require('./brand-code4.png')} />
-    </a>
-  </div>
+  <footer id={style.bottom}>
+    <div className={style.container}>
+      <a className={style.logo} href="http://code4.ro" target="_blank">
+        <img src="//code4.ro/wp-content/uploads/2017/08/logo-bw.png" alt="Code for Romania" />
+      </a>
+      <p className={style.mono}>
+          În colaborare cu Code for Romania.<br/>
+          Organizație neguvernamentală independentă, neafiliată politic și apolitică.
+      </p>
+    </div>
+  </footer>
 );
